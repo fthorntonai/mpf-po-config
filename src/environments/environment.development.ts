88e0177bd1fb2a809fd3a0371e97b8c389d43c05
fthornton67 <fthornton67@gmail.com>
2024-07-02 17:32:14 -0400

    remove security issue

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    baseUrl: '/',
+    clientId:'15a472fd-cfb5-48e7-aae0-418802be3eb1'
+
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://fthorntonai.github.io/phitrai-api-dev/',
+    clientId:'15a472fd-cfb5-48e7-aae0-418802be3eb1'
+
+};
