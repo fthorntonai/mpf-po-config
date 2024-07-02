dcaceef2145106a7d8212784bc04eb449f0ed26c
fthornton67 <fthornton67@gmail.com>
2024-07-02 17:53:25 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from './../environments/environment';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +23,13 @@ import { APP_BASE_HREF } from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule
   ],
-  providers: [ {provide: APP_BASE_HREF, useValue: environment.baseUrl}],
+  providers: [ {provide: APP_BASE_HREF, useValue: environment.baseUrl}, { provide: NZ_I18N, useValue: en_US }, provideAnimationsAsync(), provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
