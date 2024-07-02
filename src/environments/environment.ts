333da148bf9244f5b33292e44397522f32ec431b
fthornton67 <fthornton67@gmail.com>
2024-07-02 17:40:25 -0400

    update base url for prod https://fthorntonai.github.io/mpf-po-config/

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://fthorntonai.github.io/phitrai-api-dev/',
+    baseUrl: 'https://fthorntonai.github.io/mpf-po-config/',
     clientId:'15a472fd-cfb5-48e7-aae0-418802be3eb1'
 
 };
