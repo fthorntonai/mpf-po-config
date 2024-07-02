4b819ef82c58fdad0481df0195d1daff014037c9
fthornton67 <fthornton67@gmail.com>
2024-07-02 18:57:54 -0400

    add wizard module route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/wizard' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'wizard', loadChildren: () => import('./modules/wizard/wizard.module').then(m => m.WizardModule) }
 
